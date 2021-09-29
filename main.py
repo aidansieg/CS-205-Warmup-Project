85e34ba89a2598e4bdb4cae3e30f5acece27c6c4
jayce <jslesar@beta.team>
2021-09-29 14:54:45 -0400

    shouldnt break

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import pandas as pd
 
 
 DATABASE_NAME = 'president_history'
+LATEST_YEAR = 2021
+EARLIEST_YEAR = 1789
 
 
 def main():
@@ -21,16 +23,26 @@ def main():
         if input_parsing(query) is not None:
             query_structure = text_to_sql(query)
 
-            if query_structure['table'] != 'Both' or 'p for clause' in query_structure['column']:
-                data = [basic_query(DATABASE_NAME, query_structure['table'], query_structure['column'], query_structure['clause'])]
-            else:
-                data = office_query(DATABASE_NAME, query_structure['clause'])
+            try:
+                if 'clause' in query_structure.keys():
+                    print('here')
+                    print(int(query_structure['clause']))
+                    if not (EARLIEST_YEAR < int(query_structure['clause']) < LATEST_YEAR):
+                        raise(KeyError)
 
-            for d in data:
-                if isinstance(d, pd.DataFrame):
-                    print(tabulate(d, headers='keys', tablefmt='psql', showindex=False))
+                if query_structure['table'] != 'Both' or 'p for clause' in query_structure['column']:
+                    data = [basic_query(DATABASE_NAME, query_structure['table'], query_structure['column'], query_structure['clause'])]
                 else:
-                    print(d)
+                    data = office_query(DATABASE_NAME, query_structure['clause'])
+
+                for d in data:
+                    if isinstance(d, pd.DataFrame):
+                        print(tabulate(d, headers='keys', tablefmt='psql', showindex=False))
+                    else:
+                        print(d)
+            except KeyError:
+                print(f'Something went wrong with query {query}. Resulted in structure {query_structure} which could not be parsed.')
+                pass
 
 
             again = input('Would you like to query again? (Y/n) ')
